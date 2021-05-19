685637cd74c4c0d1488093d1d949fb1f2a83ba21
wangxinwang <1669499355@qq.com>
2021-05-19 17:56:43 +0800

    'feature: support dragging events'

--- index.js
+++ index.js
@@ -1,62 +1,93 @@
 export default {
   install: (app, options) => {
-    app.directive('steven-draggable', function stevenDraggableDirective(el, { value: dragzone }) {
-      el.onmousedown = function onDragStart(mouseDownEvent) {
-        // 拖动目标
-        const dragTarget = mouseDownEvent.target;
-
-        // 拖动区域 -- 默认为拖动目标的父节点
-        const dragZone = document.querySelector(dragzone) || dragTarget.parentNode;
-
-        // 初始定位 -- 用于拖动结束后恢复
-        const dragTargetOldPosition = dragTarget.style.position;
-        const dragTargetOldOffsetX = dragTarget.style.left;
-        const dragTargetOldOffsetY = dragTarget.style.top;
-
-        // 全局监听
-        document.onmousemove = function (mouseMoveEvent) {
-          let dragTargetX = mouseMoveEvent.pageX - mouseDownEvent.offsetX;
-          let dragTragetY = mouseMoveEvent.pageY - mouseDownEvent.offsetY;
-
-          const boundingRect = dragZone.getBoundingClientRect();
-
-          // X边界判断 - 左边界
-          dragTargetX = dragTargetX < boundingRect.left ? boundingRect.left : dragTargetX;
-
-          // X边界判断 - 右边界
-          if (dragTargetX + dragTarget.offsetWidth > boundingRect.right) {
-            dragTargetX = boundingRect.right - dragTarget.offsetWidth;
-          }
-
-          // Y边界判断 - 上边界
-          dragTragetY = dragTragetY < boundingRect.top ? boundingRect.top : dragTragetY;
-
-          // Y边界判断 - 下边界
-          if (dragTragetY + dragTarget.offsetHeight > boundingRect.bottom) {
-            dragTragetY = boundingRect.bottom - dragTarget.offsetHeight;
-          }
-
-          // 更新坐标
-          dragTarget.style.left = dragTargetX + 'px';
-          dragTarget.style.top = dragTragetY + 'px';
-
-          // 切换定位 position = absolute
-          dragTarget.style.position = 'absolute';
+    app.directive('steven-draggable', {
+      mounted: (el, bindings) => {
+        let dragOptions = {
+          dragZone: undefined,
+          onDragStart: undefined,
+          onDragEnd: undefined
         };
 
-        // 拖动结束
-        document.onmouseup = document.onmouseleave = function () {
-          // 恢复初始定位
-          dragTarget.style.position = dragTargetOldPosition;
-          dragTarget.style.left = dragTargetOldOffsetX;
-          dragTarget.style.top = dragTargetOldOffsetY;
+        if (typeof bindings.value === 'string') dragOptions['dragZone'] = bindings.value;
+        if (typeof bindings.value === 'object') dragOptions = bindings.value;
 
-          // 监听清理
-          document.onmousemove = null;
-          document.onmouseup = null;
-          document.onmouseleave = null;
+        el.onmousedown = function onDragStart(mouseDownEvent) {
+          // 拖动目标
+          const dragTarget = mouseDownEvent.target;
+
+          // 拖动区域 -- 默认为拖动目标的父节点
+          const dragZone = document.querySelector(dragOptions.dragZone) || dragTarget.parentNode;
+
+          // 初始定位 -- 用于拖动结束后恢复
+          const dragTargetOldPosition = dragTarget.style.position;
+          const dragTargetPointerEvents = dragTarget.style.pointerEvents;
+          const dragTargetOldOffsetX = dragTarget.style.left;
+          const dragTargetOldOffsetY = dragTarget.style.top;
+
+          // 拖动初开始状态
+          let isDragging = false;
+
+          // 拖动事件禁用
+          dragTarget.style.pointerEvents = 'none';
+
+          // 拖动开始
+          document.onmousemove = function (mouseMoveEvent) {
+            // 开始拖动
+            if (!isDragging && dragOptions.onDragStart) dragOptions.onDragStart();
+
+            isDragging = true;
+
+            let dragTargetX = mouseMoveEvent.pageX - mouseDownEvent.offsetX;
+            let dragTragetY = mouseMoveEvent.pageY - mouseDownEvent.offsetY;
+
+            const boundingRect = dragZone.getBoundingClientRect();
+
+            // X边界判断 - 左边界
+            dragTargetX = dragTargetX < boundingRect.left ? boundingRect.left : dragTargetX;
+
+            // X边界判断 - 右边界
+            if (dragTargetX + dragTarget.offsetWidth > boundingRect.right) {
+              dragTargetX = boundingRect.right - dragTarget.offsetWidth;
+            }
+
+            // Y边界判断 - 上边界
+            dragTragetY = dragTragetY < boundingRect.top ? boundingRect.top : dragTragetY;
+
+            // Y边界判断 - 下边界
+            if (dragTragetY + dragTarget.offsetHeight > boundingRect.bottom) {
+              dragTragetY = boundingRect.bottom - dragTarget.offsetHeight;
+            }
+
+            // 更新坐标
+            dragTarget.style.left = dragTargetX + 'px';
+            dragTarget.style.top = dragTragetY + 'px';
+
+            // 切换定位 position = absolute
+            dragTarget.style.position = 'absolute';
+          };
+
+          // 拖动结束
+          window.onblur = document.onmouseup = function () {
+            // 监听清理
+            document.onmousemove = window.onblur = document.onmouseup = null;
+
+            // 拖动结束
+            if (isDragging && dragOptions.onDragEnd)
+              dragOptions.onDragEnd({
+                left: dragTarget.style.left,
+                top: dragTarget.style.top
+              });
+
+            // 恢复初始定位
+            dragTarget.style.position = dragTargetOldPosition;
+            dragTarget.style.pointerEvents = dragTargetPointerEvents;
+            dragTarget.style.left = dragTargetOldOffsetX;
+            dragTarget.style.top = dragTargetOldOffsetY;
+            isDragging = false;
+          };
         };
-      };
+      },
+      unmounted: (el) => (el.onmousedown = null)
     });
   }
 };
